51f41a3e325f4760e1902b513c0ce74abe7d70ac
zylele <657345933@qq.com>
2017-10-03 00:53:47 +0800

    1.0

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,5 @@
 
-const util = require('../../utils/util.js')
+const util = require('../../utils/util.js');
 
 // pages/index/index.js
 Page({
@@ -11,20 +11,20 @@ Page({
     startDate: null,
     startWeek: null,
 
-    beforeDays: null,
+    beforeADOrBC: '',
+    beforeDate: '请输入数字进行计算',
+    beforeWeek: '',
 
-    beforeDate: "请输入数字进行计算",
-    beforeWeek: "",
-
-    afterDays: null,
-
-    afterDate: "请输入数字进行计算",
-    afterWeek: "",
+    afterADOrBC: '',
+    afterDate: '请输入数字进行计算',
+    afterWeek: '',
 
     distDate: null,
     distWeek: null,
 
-    stillDays: null
+    stillDays: 0,
+
+    showSlogan: false
   },
 
   /**
@@ -33,11 +33,10 @@ Page({
   onLoad: function (options) {
     var now = util.formatTime(new Date());
     this.setData({
-      startDate: now[0],
-      startWeek: now[1],
-      distDate: now[0],
-      distWeek: now[1],
-      stillDays: 0
+      startDate: now[1],
+      startWeek: now[2],
+      distDate: now[1],
+      distWeek: now[2]
     });
   },
 
@@ -47,33 +46,212 @@ Page({
   changeStartDate: function (e) {
     var selectDate = util.formatTime(new Date(e.detail.value));
     this.setData({
-      startDate: selectDate[0],
-      startWeek: selectDate[1],
+      startDate: selectDate[1],
+      startWeek: selectDate[2]
     });
-    var days = Number(this.data.beforeDays) || 0;
-    this.calBeforeDate(new Date(selectDate[0]), days);
+    var beforeDays = Number(this.data.beforeDays) || 0;
+    this.calBeforeDate(new Date(selectDate[1]), beforeDays);
+    var afterDays = Number(this.data.afterDays) || 0;
+    this.calAfterDate(new Date(selectDate[1]), afterDays);
   },
 
-/**
- * 改变几天之前
- */
+  /**
+   * 改变几天之前
+   */
   changeBeforeDays: function (e) {
     var days = e.detail.value;
-    this.setData({
-      beforeDays: days
-    });
+    if (typeof days == 'undefined' || days == '') {
+      days = 0;
+    }
     this.calBeforeDate(new Date(this.data.startDate), days);
   },
 
-/**
- * 计算几天之前的日期
- */
+  /**
+   * 计算几天之前的日期
+   */
   calBeforeDate: function (startDate, days) {
     var beforeDate = util.formatTime(new Date(startDate.getTime() - days * 1000 * 60 * 60 * 24));
     this.setData({
-      beforeDate: beforeDate[0],
-      beforeWeek: beforeDate[1]
+      beforeADOrBC: beforeDate[0],
+      beforeDate: beforeDate[1],
+      beforeWeek: beforeDate[2]
     });
-  }
+  },
 
+  /**
+     * 改变几天之后
+     */
+  changeAfterDays: function (e) {
+    var days = e.detail.value;
+    if (typeof days == 'undefined' || days == '') {
+      days = 0;
+    }
+    this.calAfterDate(new Date(this.data.startDate), days);
+  },
+
+  /**
+   * 计算几天之后的日期
+   */
+  calAfterDate: function (startDate, days) {
+    var afterDate = util.formatTime(new Date(startDate.getTime() + days * 86400000));
+    this.setData({
+      afterADOrBC: afterDate[0],
+      afterDate: afterDate[1],
+      afterWeek: afterDate[2]
+    });
+  },
+
+  /**
+   * 点击距离日期适当滚动
+   */
+  distDateScroll: function () {
+    wx.pageScrollTo({
+      scrollTop: 70
+    })
+  },
+
+  /**
+   * 改变距离日期
+   */
+  changeDistDate: function (e) {
+    var selectDate = new Date(e.detail.value);
+    var distDate = util.formatTime(selectDate);
+    var now = util.formatTime(new Date());
+    var stillDays = (new Date(distDate[1]).getTime() - new Date(now[1]).getTime()) / 86400000;
+
+    this.setData({
+      distDate: distDate[1],
+      distWeek: distDate[2],
+      stillDays: stillDays
+    });
+
+    wx.pageScrollTo({
+      scrollTop: 0
+    });
+
+    // 检测彩蛋
+    this.checkDistDateEgg(selectDate);
+  },
+
+  /**
+   * 距离日期彩蛋
+   */
+  checkDistDateEgg: function (date) {
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    if (month == 7 && day == 6) {//乐乐生日
+      var year = date.getFullYear();
+      var nowYear = new Date().getFullYear();
+      if (year > nowYear && year <= 2092) {
+        wx.showModal({
+          title: '这一天有大事',
+          content: '提前祝计日指期开发者乐乐生日快乐',
+          showCancel: false
+        });
+      }
+    } else if (month == 7 && day == 31) {//毛毛生日
+      var year = date.getFullYear();
+      var nowYear = new Date().getFullYear();
+      if (year > nowYear && year <= 2092) {
+        wx.showModal({
+          title: '一个彩蛋',
+          content: '这一天有大事！',
+          confirmText: '什么大事',
+          showCancel: false,
+          success: function (res) {
+            wx.showModal({
+              title: '',
+              content: 'L:提前祝毛毛生日快乐！想买啥？',
+              confirmText: '我想想',
+              showCancel: false,
+              success: function (res) {
+                wx.showLoading({
+                  title: '毛毛抓耳挠腮',
+                  mask: true
+                });
+                setTimeout(function () {
+                  wx.hideLoading();
+                  wx.showModal({
+                    title: '',
+                    content: 'L:还没想好吗？',
+                    confirmText: '好惹！',
+                    showCancel: false,
+                    success: function (res) {
+                      wx.showModal({
+                        title: '',
+                        content: 'xx口红xx霜xxx神仙水',
+                        confirmText: '就这些吧',
+                        showCancel: false,
+                        success: function (res) {
+                          wx.showModal({
+                            title: '',
+                            content: 'L:再加点吧',
+                            confirmText: '怎好意思',
+                            showCancel: false,
+                            success: function (res) {
+                              wx.showModal({
+                                title: '',
+                                content: '那iPhone X(作者注：这里X指' + year + '年出的型号)',
+                                confirmText: '这下够了',
+                                showCancel: false,
+                                success: function (res) {
+                                  wx.showModal({
+                                    title: '',
+                                    content: 'L:搞一波大的吧，Tesla MODEL X 怎么样',
+                                    confirmText: '嘻嘻爱你',
+                                    showCancel: false,
+                                    success: function (res) {
+                                      wx.showLoading({
+                                        title: '正在发送毛毛的爱',
+                                        mask: true
+                                      });
+                                      setTimeout(function () {
+                                        wx.showToast({
+                                          title: '发送成功',
+                                          icon: 'success',
+                                          duration: 2000
+                                        });
+                                      }, 3000);
+                                    }
+                                  });
+                                }
+                              });
+                            }
+                          });
+                        }
+                      });
+                    }
+                  });
+                }, 3000);
+              }
+            });
+          }
+        });
+      }
+    }
+  },
+
+  /**
+   * 触摸彩蛋
+   */
+  showSlogan: function () {
+    this.setData({
+      showSlogan: true
+    });
+  },
+  hideSlogan: function () {
+    this.setData({
+      showSlogan: false
+    });
+  },
+
+  /**
+   * 分享
+   */
+  onShareAppMessage: function (res) {
+    return {
+      title: '时间，时间会给我答案',
+      path: '/pages/index/index'
+    }
+  }
 })
--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,6 @@
+const B_C = "公元前";
+const A_D = "公元";
+
 const weeks = [
   "星期日",
   "星期一",
@@ -6,21 +9,40 @@ const weeks = [
   "星期四",
   "星期五",
   "星期六",
-]
+];
 
 const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const dateStr = [year, month, day].map(formatNumber).join('-');
-  const week = weeks[date.getDay()];
-  return new Array(dateStr, week);
-}
+  var ADOrBC = "";
+  var year = date.getFullYear();
 
-const formatNumber = n => {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
+  /**
+   * 判断公元前还是公元后
+   * 公元没有0年，只有公元1年
+   * 所以公元前的在数字上应往前推算一年
+   */
+  if (year > 0) {
+    ADOrBC = A_D;
+  } else {
+    year--;
+    year = Math.abs(year);
+    ADOrBC = B_C;
+  }
+  
+  year = year.toString();
+  while (!year[3]) {
+    year = "0" + year;
+  }
+
+  var month = date.getMonth() + 1;
+  month = month.toString();
+  month = month[1] ? month : "0" + month;
+
+  var day = date.getDate();
+  day = day.toString();
+  day = day[1] ? day : "0" + day;
+
+  return new Array(ADOrBC, [year, month, day].join('-'), weeks[date.getDay()]);
+};
 
 module.exports = {
   formatTime: formatTime
